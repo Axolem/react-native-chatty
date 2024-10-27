fa8017f42fa92a9cfd5a4e373c90f6b7202b6489
Axole Maranajana <axolemar@gmail.com>
2024-10-27 12:35:28 +0200

    rename files

--- src/utils/photoView.ts
+++ src/utils/photoView.ts
@@ -1,10 +1,10 @@
 let PhotoView: any;
 
 try {
-  PhotoView = require('@muhammedkpln/react-native-image-viewing').default;
+  PhotoView = require('@trial-trl/react-native-image-viewing').default;
 } catch {
   console.warn(
-    '@muhammedkpln/react-native-image-viewing not found. Image browser/zoom will not work.'
+    '@trial-trl/react-native-image-viewing not found. Image browser/zoom will not work.'
   );
 }
 
